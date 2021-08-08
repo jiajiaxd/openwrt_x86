a0d38715b6d41eff493a11b8119c2d6783c88c7f
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2021-08-08 15:08:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-cd lede/package
+cd package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 cd lean
 rm -rf luci-theme-argon  
