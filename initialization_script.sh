69c9788fb6383fe40869737f0a985950b175c564
Charles Jia <i@jiajiaxd.com>
2023-03-15 16:23:01 +0800

    Update initialization_script.sh

--- initialization_script.sh
+++ initialization_script.sh
@@ -1,5 +1,4 @@
 sudo apt update -y
-sudo apt full-upgrade -y
 sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
 git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
